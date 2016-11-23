ae3049d84dded644fd5f9763ea81c1564654981f
guiBerger <gberger.eclille@gmail.com>
2016-11-23 11:11:10 -0500

    Removing some weird lines

--- base_script.py
+++ base_script.py
@@ -14,12 +14,9 @@ index_train, index_valid, index_test = preprocessing.split_dataset(dataset)
 
 # Define the model
 model = models.get_LSTM_v1(params.T-1, params.D-1, params.LR, params.NHIDDEN, params.NNEURONSH, params.DROPOUT_RATE)
-#export PATH=anaconda2/bin/:$PATH
-#export PYTHONPATH=anaconda2/bin/:$PYTHONPATH
-export PATH=/u/dift3395/.local/bin:$PATH
-export PYTHONPATH=/u/dift3395/.local/lib/python2.7/site-packages
 #model = models.get_CausalCNN_v1(params.T-1, params.D-1, params.LR, params.DROPOUT_RATE)
 
 # Training
 tr.training(params.PATH_EXPERIMENT, model, dataset, index_train, index_valid,
-            params.D, params.B, params.NB_SAMPLES_PER_EPOCH, params.NB_EPOCHS, params.PATIENCE)
+            params.D, params.B, params.NB_SAMPLES_PER_EPOCH, params.NB_EPOCHS, params.PATIENCE,
+            pretrained=params.PRETRAINED, h5py=params.H5PY)
