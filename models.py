fce87fad9c2b1f2131577c9a6da6c567d2c61bc9
Arnaud Augustin <augustar@leto52.iro.umontreal.ca>
2016-12-02 14:28:04 -0500

    add of get_GRU_v1

--- models.py
+++ models.py
@@ -27,6 +27,28 @@ def get_LSTM_v1(T, D, lr, nhidden, nneuronsh, drop_rate):
 
     return model
 
+def get_GRU_v1(T, D, lr, nhidden, nneuronsh, drop_rate):
+    # Input layer
+    inputs = Input((T, D))
+    # Masking "only-0" input features
+    masked = Masking(mask_value=0.0)(inputs)
+    # Hidden layers
+    for i in range(nhidden):
+        if i == 0:
+            hidden  = GRU(nneuronsh, return_sequences=True)(masked)
+        else:
+            hidden  = GRU(nneuronsh, return_sequences=True)(dropout)
+        dropout = Dropout(drop_rate)(hidden)
+    # Output layer : linear TimeDistributedDense + softmax
+    decoder = TimeDistributed(Dense(D))(dropout) # Apply the same dense layer on each timestep
+    outputs = Activation("softmax") (decoder)
+
+    model = Model(input=inputs, output=outputs)
+
+    model.compile(optimizer=Adam(lr=lr), loss="categorical_crossentropy")
+
+    return model
+
 def CausalConvolution1D(input_layer, nfilters, filter_length, atrous_rate=1, activation="linear", batch_norm=True,
                         **kwargs):
     total_length = filter_length + (filter_length-1)*(atrous_rate-1)
@@ -148,19 +170,19 @@ def get_CausalCNN_v5(T, D, lr, drop_rate):
     # Input layer
     inputs = Input((T, D))
     # First block : CausalCNN layers
-    hidden  = CausalConvolution1D(inputs, 128, 3, atrous_rate=1, activation="tanh")
+    hidden  = CausalConvolution1D(inputs, 256, 3, atrous_rate=1, activation="tanh")
     for i in range(3):
-        hidden  = CausalConvolution1D(hidden, 256, 3, atrous_rate=1, activation="linear", batch_norm=False)
-        hidden  = CausalConvolution1D(hidden, 128, 3, atrous_rate=2**(i+1), activation="tanh")
+        hidden  = CausalConvolution1D(hidden, 512, 3, atrous_rate=1, activation="linear", batch_norm=False)
+        hidden  = CausalConvolution1D(hidden, 256, 3, atrous_rate=2**(i+1), activation="tanh")
     # Second block : Residual layers
     for i in range(5):
         # Causal CNN layers :
-        tanh_hidden  = CausalConvolution1D(hidden, 256, 5, atrous_rate=2**(i+1), activation="tanh")
-        sigm_hidden  = CausalConvolution1D(hidden, 256, 5, atrous_rate=2**(i+1), activation="sigmoid")
+        tanh_hidden  = CausalConvolution1D(hidden, 512, 5, atrous_rate=2**(i+1), activation="tanh")
+        sigm_hidden  = CausalConvolution1D(hidden, 512, 5, atrous_rate=2**(i+1), activation="sigmoid")
         # Merge : tanh * sigmoid (see WaveNet paper)
         activ = merge([tanh_hidden, sigm_hidden], mode="mul")#, output_shape=(160,128))
         # Convolution 1x1 to reduce complexity
-        projection = Convolution1D(128, 1, activation='linear')(activ)
+        projection = Convolution1D(256, 1, activation='linear')(activ)
         # Residual merge
         hidden = merge([hidden, projection], mode="sum")
     # Last block : linear TimeDistributedDense + softmax
